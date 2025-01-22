ff49026a7ae943fb76cc3c5516292c4aafd7411e
Alessio Mezzina <103206184+timeassassinRG@users.noreply.github.com>
2025-01-22 18:58:39 +0100

    commit

--- assets/js/script.js
+++
@@ -1,8 +0,0 @@
-// Imposta l'anno corrente nel footer
-document.addEventListener("DOMContentLoaded", function() {
-    const yearSpan = document.getElementById("annoCorrente");
-    if (yearSpan) {
-      yearSpan.textContent = new Date().getFullYear();
-    }
-  });
-  
